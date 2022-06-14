70c8bbf5abdc5565516ad78929abcd6b1f2ff986
André <andre.or.luuiz@gmail.com>
2022-06-14 11:15:23 -0300

    heroku docker

---
+++ node-s/Http/Controllers/QueueController.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const Queue = require('../Models/Queue');
+
+class QueueController {
+
+    /**
+     * Método para criar mensagem
+     * @param {express.Request} req
+     * @param {express.Response} res
+     * @param {express.NextFunction} next
+     */
+    static async criarMensagem(req, res, next) {
+        try {
+            // Criação de objeto queue
+            const queue = new Queue();
+            // Recuperação do body
+            const body = req.body;
+            // Criar conexao da queue
+            await queue.criarConexao();
+            // Envio do body para a fila
+            await queue.enviarMensagem('default', body);
+            // Response
+            res.status(200).send({ sucess: true, message: 'Mensagem enviada a fila' });
+        } catch (error) {
+            // Response Error
+            res.status(500).send({ message: error.message, stack: error.stack });
+        }
+    }
+
+    /**
+     * Método para consumir mensagens
+     * @param {express.Request} req
+     * @param {express.Response} res
+     * @param {express.NextFunction} next
+     */
+    static async consumirMensagens(req, res, next) {
+        try {
+            // Criação de objeto queue
+            const queue = new Queue();
+            // Recuperação do body
+            const body = req.body;
+            // Criar conexao da queue
+            await queue.criarConexao();
+            // Envio do nome da fila
+            await queue.consumirMensagem(body.queue);
+            // Response retorno
+            res.status(200).send({ sucess: true, message: 'Leitura em andamento' });
+        } catch (error) {
+            // Response Error
+            res.status(500).send({ message: error.message, stack: error.stack });
+        }
+    }
+}
+module.exports = QueueController;
---
+++ node-s/Http/Models/Queue.js
@@ -0,0 +1,103 @@
+// const amqp = require('amqplib');
+const amqp = require('amqplib');
+const amqp_cb = require('amqplib/callback_api');
+
+class Queue {
+    /** @type {amqp.Connection} */
+    connection;
+    /** @type {amqp.Channel} */
+    channel;
+    /** @type {string} */
+    queue;
+    /** @type {any} */
+    message;
+
+    // Definição do constructor
+    constructor() { }
+
+    /**
+     * Método para abrir a conexão
+     */
+    async criarConexao() {
+        // Abrindo a conexão
+        this.connection = await amqp.connect(process.env.FILA_ENDPOINT);
+    }
+
+    /**
+     * Método para realizar o envio da mensagem
+     * @param {string} queue
+     * @param {any} message
+     */
+    async enviarMensagem(queue, message) {
+        // Definição de variável
+        this.queue = queue;
+        this.message = JSON.stringify(message);
+        // Procedimentos AMQP
+        await this.criarCanal();
+        await this.enfileirar();
+        await this.fecharConexao();
+    }
+
+    /**
+     * Consumir Mensagem
+     */
+    async consumirMensagem(queue) {
+        // Seta a fila como uma propriedade
+        this.queue = queue;
+        // Cria o canal de comunicação
+        await this.criarCanal();
+        // Log de mensagens dentro da queue
+        console.log(" [*] Aguardando por mensagens em %s.", this.queue);
+        // Consumo de mensagem
+        await this.consumir();
+        await this.fecharConexao();
+    }
+
+    /**
+     *  Método para consumir as mensagens no canal
+     */
+    async consumir() {
+        console.log(' [*] Inicio de consumo');
+        await this.channel.consume(this.queue, (msg) => {
+            if (msg !== null) {
+                this.logMessage(msg);
+                this.channel.ack(msg);
+            }
+        }, { noAck: true });
+    }
+
+    /**
+     * Logar mensagem lida
+     */
+    logMessage(msg) {
+        console.log(' [x] Nova mensagem: %s ', msg.content.toString());
+    }
+
+    /**
+     * Cria um canal de comunicação AMQP para produzir mensagens
+     */
+    async criarCanal() {
+        this.channel = await this.connection.createChannel();
+        // Canal na fila x
+        await this.channel.assertQueue(this.queue, { durable: false });
+    }
+
+    /**
+     * Insere a mensagem na fila definida
+     */
+    async enfileirar() {
+        // Envio para fila
+        this.channel.sendToQueue(this.queue, Buffer.from(this.message));
+        // Mensagem enviada
+        console.log(" [x] Enviada %s", this.message);
+    }
+
+    /**
+     * Método que fecha a conexão
+     */
+    async fecharConexao() {
+        await this.channel.close();
+        await this.connection.close();
+    }
+}
+module.exports = Queue;
